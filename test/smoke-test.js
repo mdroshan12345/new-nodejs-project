00593aacc0717ea93267731cb65a727f3f046e5e
mdroshan12345 <mdroshan30@gmail.com>
2024-01-30 16:13:12 +0530

    smoke4

--- test/smoke-test.js
+++ test/smoke-test.js
@@ -1,23 +1,31 @@
 const assert = require('assert');
 const http = require('http');
 
+//describe('Node.js Server', function () {
+    // Start the server before tests
+    //before(function () {
+      //require('../your-server-script.js');
+    //});
+  
 
-  it('should respond with "node js hello world !!!"', function (done) {
-    http.get('http://127.0.0.1:8000', function (res) {
-      let data = '';
 
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
+ // it('should respond with "node js hello world !!!"', function (done) {
+   // http.get('http://127.0.0.1:8000', function (res) {
+      //let data = '';
+
+      //res.on('data', function (chunk) {
+       // data += chunk;
+     // });
 
       res.on('end', function () {
         assert.strictEqual(data.trim(), 'node js hello world !!!');
         done();
       });
-    });
-  });
+  //  });
+ // });
 
   // Stop the server after tests
   after(function () {
     // Implement server shutdown logic if needed
   });
+//});
